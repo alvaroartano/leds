6761179d4d4118204b781195dd0ee5f0df9e7dfa
Álvaro Artano <alvaroartano@gmail.com>
2020-09-02 10:33:12 +0200

    Add files via upload

---
+++ tutorial.php
@@ -0,0 +1,37 @@
+<?php 
+
+$keys = array("RmOl8i0vzqCfaxji","mR7MVqyyuLF8Atl4","VRhZSg7sUP7VGj1I","oPeZXvRXrGRmLqB0","rY0pDDPvWwucTUdo","lLCmvzfrIMpFTrcP","lv9HvuEzliH5heTU","KiPM7Y7UIiffdAHG","umDDAwurDaFdQdFG","u8kYgf0zWCjz2F03","WFNGxzX5pWOsZym6","T3EnSTefQd8TKxxy","jW3g37dDc0XDXYjM","Ks1ZNu9lHFMbT2Jj","3OcRstbZZ4EkXeHs","zxYcrEQwN02zrIgf","PzP20hN4O6hryv3W","TvUj33ZfjIpmbY7A","adwVq3a7OVZyHgAG","nh0g8Q9n0lB5dJRj","5F0y9hTStOp1kRiv","Rak1U2vvKH5kmnJm","N9bU8CQQh4Tk2MBq","GUlGReNbQImT1GVE","HzaCcOq2YuFt171z","oAkCQZcKQoO2aUS5","Tu1hx8qq5VYZn6Gg","cbHrXKGv3TwxVsVJ","IMEoyg90dwjgfnDB","Qwwi8UrSkBLr4nlp","m7gh1NwLeyg65tP4","5jIYq52qkHxEufYC","O9rL0QzvOoSv75Po","6Su198oQV7f6WAiz","74LXzCE9jFJovp21","yBgPy104aNf3bg0A","HD8CGBF26i0Wz6K7","GJgdlaa8U2U1y3vK","EebzrB4CWzJtWTdf","4bSfx5re9Rl8DewZ","Fz0hYN8Kv5K8JEkB","tOwWU4Ra53b8Ul4K","GtaNocupAPmcgVpo","hqFyI4w1A1mW3rSC","kex0vx5VFBYHcBJI","1Pb52XzUSBPDEYuB","RwMrAdTJkSdJ6qyN","IRIfDkeAuZPOxDVA","oflww7H5KUKfZsJW","IlZipNJdwirvjEeE","okiY75tkL5GvKjEZ","m7dHpMd0568UTPCA","b9sMvRMiUgj1OvkI","MDhjrW4bCodjuZFQ","F5VdAFfQDrDjNYSt","0U55Fd410fx0ub9t","8Qbsxu0W5nGH0UE9","S11RX7MvnwikPCNj","9R8hUdC76ZYNGI1I","CdBH1joM04iYVrbq","JvnAtLr1MnoBjmas","3Lzly8VHSlMbcu0T","jYrGGsHw2t7fxCIc","8X01PZqUBroJnCaH","nAhOj75wUO9oeqIU","C72aHdz7rRdzRZif","92wD63TS0E97asK8","1PznsoEg7lCKlaTg","txS0tcjdf1d8MNpq","AqfYK3iyElawps8k","vKXr6HDqqKPbG1YI","lygX9JyrDlAYfWbN","LLnFCRNp2oDsxvSj","ejAAh2gS0xyuzlTQ","RpbAYGtKJpJvhtk5","ccDNLhBxlyIaMDsJ","CSGCIMhPModj9KKc","p16PHNXGKdnWbZR6","uiAg87uBlHpEY1uJ","ZD6VI2Bt7io5GLFj","YXQdMSNjGQEyizZk","E4EENe3JRYGQcRPR","4ic6CqArBCSb4gCV","5uYfT3kL9LwOfjEa","RJR7v7tPzRlOJxV3","KFszJtqK5WyrqYRv","D72SfQn1Vc6Mt9Mt","2LiDHhivJNOJfbuT","znL4SbghiJEX6Qhk","QPC2jdgpNOd8DqHi","GmNaK12W6pzzi0en","OJKfsN7SIThwCPHQ","5ZmbSoAOTXfh3SwB","8b7ErJ38QW8TyOy7","N24ROL8VumMHMXxa","702yLwjGzejvQeAA","6WXiD6XlRLa49ujJ","gHvUOF8JZUu4rA75","oo0GyDnvJPjBAaT7","GEOeEXdsms9NBiSm");
+
+$key = $_POST['key'];
+
+if (in_array($key, $keys)) {
+    echo '<head>';
+    echo '';
+    echo '<title>Instructions for making led lights :)</title>';
+    echo '';
+    echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
+    echo '<link rel="stylesheet" href="style.css">';
+    echo '';
+    echo '</head>';
+    echo '';
+    echo '<body>';
+    echo '';
+    echo '';
+    echo '';
+    echo "<object data='/lucesss/tutorial.pdf'";
+    echo "type='application/pdf'";
+    echo "width='100%'";
+    echo "height='100%'>";
+    echo '<p>This browser does not support inline PDFs. Please download the PDF to view it: <a href="/tutorial.pdf">Download PDF</a></p>';
+    echo '</object>';
+    echo '';
+    echo '';
+    echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>';
+    echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
+    echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
+    echo '';
+    echo '</body>';}
+else{
+    echo 'I am sorry! That key is incorrect! Try it again!';}
+?>
+
